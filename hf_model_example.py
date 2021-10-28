8a36908de7c749ccc1ff60d487361ecf67b6e7d7
Wieceslaw <slava.astra@yahoo.com>
2021-10-28 08:46:02 +0300

    Update hf_model_example

--- hf_model_example.py
+++ hf_model_example.py
@@ -2,7 +2,7 @@ from transformers import BertTokenizer, FlaxBertModel
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = FlaxBertModel.from_pretrained('bert-base-uncased')
-inputs = tokenizer("Hello, my dog is cute", return_tensors="np")
+inputs = tokenizer("Hello, my dog is cute", return_tensors='jax')
 outputs = model(**inputs)
-print("Prediction logits: {}".format(outputs.prediction_logits))
-print("Seq relationship logits: {}".format(outputs.seq_relationship_logits))
+print(outputs)
+print(outputs.__dict__)
