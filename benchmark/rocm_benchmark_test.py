ed981cbf53ee774ac026201b7de992893030adf1
alexpetersburg <topoman121@gmail.com>
2021-11-11 02:54:46 +0300

    -add model to benchmark

--- benchmark/rocm_benchmark_test.py
+++ benchmark/rocm_benchmark_test.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
 
     tokenizer = GPT2Tokenizer.from_pretrained('EleutherAI/gpt-neo-1.3B')
     model = FlaxGPTNeoModel.from_pretrained('EleutherAI/gpt-neo-1.3B')
+    print('here')
     args = ROCmBenchmarkArguments(model=model, batch_sizes=[8], sequence_lengths=[8, 32, 128, 512],)
     benchmark = ROCmBenchmark(args)
     results = benchmark.run()
