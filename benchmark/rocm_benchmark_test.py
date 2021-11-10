8d5bab75fed848eb30b81d77e9bb92b842988a24
alexpetersburg <topoman121@gmail.com>
2021-11-10 21:20:23 +0300

    -change test model

--- benchmark/rocm_benchmark_test.py
+++ benchmark/rocm_benchmark_test.py
@@ -1,7 +1,7 @@
 from rocm_benchmark import ROCmBenchmark, ROCmBenchmarkArguments
 
 if __name__ == '__main__':
-    args = ROCmBenchmarkArguments(models=["bert-base-uncased"], batch_sizes=[8], sequence_lengths=[8, 32, 128, 512])
+    args = ROCmBenchmarkArguments(models=['EleutherAI/gpt-neo-1.3B'], batch_sizes=[8], sequence_lengths=[8, 32, 128, 512])
     benchmark = ROCmBenchmark(args)
     results = benchmark.run()
     print(results)
