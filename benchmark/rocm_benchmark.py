e9c20b2076e2818c37e46c526c3ae4647ebe72b4
alexpetersburg <topoman121@gmail.com>
2021-11-10 21:35:00 +0300

    -debug

--- benchmark/rocm_benchmark.py
+++ benchmark/rocm_benchmark.py
@@ -112,6 +112,7 @@ class ROCmBenchmark(Benchmark):
     def _inference_memory(
         self, model_name: str, batch_size: int, sequence_length: int
     ) -> [Memory, Optional[MemorySummary]]:
+        print('!!!!! start inference')
         _inference = self._prepare_inference_func(model_name, batch_size, sequence_length)
         return self._measure_memory(_inference)
 
