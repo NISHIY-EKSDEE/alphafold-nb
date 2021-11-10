aba2d5f8ab962af23c0a3abf78569bf1583c463d
alexpetersburg <topoman121@gmail.com>
2021-11-10 21:14:04 +0300

    -fix import

--- benchmark/rocm_benchmark_test.py
+++ benchmark/rocm_benchmark_test.py
@@ -1,4 +1,4 @@
-from .rocm_benchmark import ROCmBenchmark, ROCmBenchmarkArguments
+from rocm_benchmark import ROCmBenchmark, ROCmBenchmarkArguments
 
 if __name__ == '__main__':
     args = ROCmBenchmarkArguments(models=["bert-base-uncased"], batch_sizes=[8], sequence_lengths=[8, 32, 128, 512])
