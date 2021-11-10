73d6c03eeb4fc6924249b680b5e742ba48c7b36a
alexpetersburg <topoman121@gmail.com>
2021-11-10 21:19:11 +0300

    -fix import

--- benchmark/rocm_benchmark.py
+++ benchmark/rocm_benchmark.py
@@ -26,6 +26,75 @@ key = random.PRNGKey(0)
 logger = logging.get_logger(__name__)
 
 
+@dataclass
+class ROCmBenchmarkArguments(BenchmarkArguments):
+
+    deprecated_args = [
+        "no_inference",
+        "no_cuda",
+        "no_tpu",
+        "no_speed",
+        "no_memory",
+        "no_env_print",
+        "no_multi_process",
+    ]
+
+    def __init__(self, **kwargs):
+        """
+        This __init__ is there for legacy code. When removing deprecated args completely, the class can simply be
+        deleted
+        """
+        for deprecated_arg in self.deprecated_args:
+            if deprecated_arg in kwargs:
+                positive_arg = deprecated_arg[3:]
+                setattr(self, positive_arg, not kwargs.pop(deprecated_arg))
+                logger.warning(
+                    f"{deprecated_arg} is depreciated. Please use --no_{positive_arg} or {positive_arg}={kwargs[positive_arg]}"
+                )
+
+        self.torchscript = kwargs.pop("torchscript", self.torchscript)
+        self.torch_xla_tpu_print_metrics = kwargs.pop("torch_xla_tpu_print_metrics", self.torch_xla_tpu_print_metrics)
+        self.fp16_opt_level = kwargs.pop("fp16_opt_level", self.fp16_opt_level)
+        super().__init__(**kwargs)
+
+    torchscript: bool = field(default=False, metadata={"help": "Trace the models using torchscript"})
+    torch_xla_tpu_print_metrics: bool = field(default=False, metadata={"help": "Print Xla/PyTorch tpu metrics"})
+    fp16_opt_level: str = field(
+        default="O1",
+        metadata={
+            "help": (
+                "For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']. "
+                "See details at https://nvidia.github.io/apex/amp.html"
+            )
+        },
+    )
+
+    @cached_property
+    def _setup_devices(self) -> Tuple["jax.device", int]:
+        return jax.devices()[0], len(jax.devices())
+
+    @property
+    def is_tpu(self):
+        return False and self.tpu
+
+    @property
+    def device_idx(self) -> int:
+        # TODO(PVP): currently only single GPU is supported
+        return jax.devices()[0]
+
+    @property
+    def device(self) -> "jax.device":
+        return self._setup_devices[0]
+
+    @property
+    def n_gpu(self):
+        return self._setup_devices[1]
+
+    @property
+    def is_gpu(self):
+        return self.n_gpu > 0
+
+
 class ROCmBenchmark(Benchmark):
 
     args: ROCmBenchmarkArguments
@@ -228,70 +297,4 @@ class ROCmBenchmark(Benchmark):
             return "N/A", None
 
 
-@dataclass
-class ROCmBenchmarkArguments(BenchmarkArguments):
 
-    deprecated_args = [
-        "no_inference",
-        "no_cuda",
-        "no_tpu",
-        "no_speed",
-        "no_memory",
-        "no_env_print",
-        "no_multi_process",
-    ]
-
-    def __init__(self, **kwargs):
-        """
-        This __init__ is there for legacy code. When removing deprecated args completely, the class can simply be
-        deleted
-        """
-        for deprecated_arg in self.deprecated_args:
-            if deprecated_arg in kwargs:
-                positive_arg = deprecated_arg[3:]
-                setattr(self, positive_arg, not kwargs.pop(deprecated_arg))
-                logger.warning(
-                    f"{deprecated_arg} is depreciated. Please use --no_{positive_arg} or {positive_arg}={kwargs[positive_arg]}"
-                )
-
-        self.torchscript = kwargs.pop("torchscript", self.torchscript)
-        self.torch_xla_tpu_print_metrics = kwargs.pop("torch_xla_tpu_print_metrics", self.torch_xla_tpu_print_metrics)
-        self.fp16_opt_level = kwargs.pop("fp16_opt_level", self.fp16_opt_level)
-        super().__init__(**kwargs)
-
-    torchscript: bool = field(default=False, metadata={"help": "Trace the models using torchscript"})
-    torch_xla_tpu_print_metrics: bool = field(default=False, metadata={"help": "Print Xla/PyTorch tpu metrics"})
-    fp16_opt_level: str = field(
-        default="O1",
-        metadata={
-            "help": (
-                "For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']. "
-                "See details at https://nvidia.github.io/apex/amp.html"
-            )
-        },
-    )
-
-    @cached_property
-    def _setup_devices(self) -> Tuple["jax.device", int]:
-        return jax.devices()[0], len(jax.devices())
-
-    @property
-    def is_tpu(self):
-        return False and self.tpu
-
-    @property
-    def device_idx(self) -> int:
-        # TODO(PVP): currently only single GPU is supported
-        return jax.devices()[0]
-
-    @property
-    def device(self) -> "jax.device":
-        return self._setup_devices[0]
-
-    @property
-    def n_gpu(self):
-        return self._setup_devices[1]
-
-    @property
-    def is_gpu(self):
-        return self.n_gpu > 0
