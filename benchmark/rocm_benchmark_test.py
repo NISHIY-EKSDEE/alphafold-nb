ff24260cd71183bae6d16311c9dbb6880d816314
alexpetersburg <topoman121@gmail.com>
2021-11-11 02:15:19 +0300

    -add model to benchmark

--- benchmark/rocm_benchmark_test.py
+++ benchmark/rocm_benchmark_test.py
@@ -2,10 +2,11 @@ from rocm_benchmark import ROCmBenchmark, ROCmBenchmarkArguments
 
 if __name__ == '__main__':
     from transformers import GPT2Tokenizer, FlaxGPTNeoModel
-    from python_smi_tools.rocm_smi import getMaxPower
+    from python_smi_tools.rocm_smi import getMaxPower, showMemUse
     import jax
     print(jax.devices()[0])
-    print(getMaxPower("0"))
+    print(getMaxPower(0))
+    print(showMemUse([0]))
     tokenizer = GPT2Tokenizer.from_pretrained('EleutherAI/gpt-neo-1.3B')
     model = FlaxGPTNeoModel.from_pretrained('EleutherAI/gpt-neo-1.3B')
     args = ROCmBenchmarkArguments(model=model, batch_sizes=[8], sequence_lengths=[8, 32, 128, 512],
