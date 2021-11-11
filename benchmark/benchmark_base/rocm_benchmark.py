933c1d8338cd2fb24d758741923ac8ec12edd17f
alexpetersburg <topoman121@gmail.com>
2021-11-11 16:36:03 +0300

    -add base benchmark

--- benchmark/benchmark_base/rocm_benchmark.py
+++ benchmark/benchmark_base/rocm_benchmark.py
@@ -1,7 +1,8 @@
+from __future__ import absolute_import
 import timeit
 from multiprocessing import Pipe, Process
 import jax
-from benchmark.python_smi_tools.rocm_smi import getMemInfo, initializeRsmi
+from ..python_smi_tools.rocm_smi import getMemInfo, initializeRsmi
 
 
 def print_result(memory, compute_time):
